2d9bb52b9d1088a6d9c5e9838fce9a5e509ba061
Steve Kondik <shade@chemlab.org>
2013-12-28 15:48:20 -0800

    build: Add QCOM target-specific config

--- core/config.mk
+++ core/config.mk
@@ -962,4 +962,7 @@ endif
 ## last, to avoid accidental resetting by device configs
 $(eval include vendor/nitrogen/sepolicy/sepolicy.mk)
 
+# Rules for QCOM targets
+include $(TOPDIR)vendor/nitrogen/config/qcom_target.mk
+
 include $(BUILD_SYSTEM)/dumpvar.mk
